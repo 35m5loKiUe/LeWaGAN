76c41aa1e77f990f9290e8dd2d39af63e6ab57f6
theo pinto--dalle <pinto.theo@icloud.com>
2023-02-09 20:46:07 +0100

    Ran test in dataset_creation.ipynb and Implemented the create_dataset function in the preprocessing.py to add normalization and batch instanciation

--- LeWaGAN/MLOps/preprocessing.py
+++ LeWaGAN/MLOps/preprocessing.py
@@ -2,19 +2,26 @@ import tensorflow
 
 
 #Create a dataset from a local directory
-def create_dataset(path, image_size=(128, 128), labels=None, color_mode='rgb', batch_size=32,
+def create_dataset(path, image_size=(128, 128), batch_size=32, color_mode='rgb',
                      validation_split=0.) :
     """This function create a dataset for tensorflow from a directory with images
     arguments :
     path : path of the directory (not the path of images inside the directory !!)
     image_size : (tuples like (28,28)) image_size
-    labels : None for non-classification tasks (keep it to none)
     color_mode : 'rgb', 'grayscale' or 'rbga', 'rgb' by default
-    batch_size : batch size for the dataset
     validation_split : float between 0 and 1
     """
 
+    #Instanciate dataset
     dataset = tensorflow.keras.utils.image_dataset_from_directory(path=path, color_mode=color_mode,
-                                           labels=labels, image_size=image_size,
-                                           batch_size=batch_size, validation_split=validation_split)
-    return dataset
+                                           labels=None, image_size=image_size,
+                                           batch_size=None, validation_split=validation_split)
+
+    #Normalization / Scaling
+    func = lambda x : x/255
+    n_dataset = dataset.map(map_func=func)
+
+    #Batch the dataset with specified batch_size
+    f_dataset = n_dataset.batch(batch_size=batch_size, drop_remainder=False)
+
+    return f_dataset
