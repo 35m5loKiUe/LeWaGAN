0e72d2b02d1f53406a13111b67192afdf5de751a
Guillaume Dumont <35m5loKiUe@gmail.com>
2023-02-11 15:26:37 +0100

    added global params to preproc function

--- LeWaGAN/MLOps/params.py
+++ LeWaGAN/MLOps/params.py
@@ -1,10 +1,10 @@
 import os
 
 # model params
-IMAGE_SIZE = os.environ.get('IMAGE_SIZE')
-EPOCHS = os.environ.get('EPOCHS')
-BATCH_SIZE = os.environ.get('BATCH_SIZE')
-NOISE_DIM = os.environ.get('NOISE_DIM')
+IMAGE_SIZE = int(os.environ.get('IMAGE_SIZE'))
+EPOCHS = int(os.environ.get('EPOCHS'))
+BATCH_SIZE = int(os.environ.get('BATCH_SIZE'))
+NOISE_DIM = int(os.environ.get('NOISE_DIM'))
 
 # Data locations
 ROOT_PATH = os.environ.get('ROOT_PATH')
@@ -12,3 +12,4 @@ PREPROC_LOCATION = os.environ.get('PREPROC_LOCATION')
 CHECKPOINT_PATH = os.environ.get('CHECKPOINT_PATH')
 DATA_PATH = os.environ.get('DATA_PATH')
 PREPROC_PATH = os.environ.get('PREPROC_PATH')
+DATA_LOCATION = os.environ.get('DATA_LOCATION')
--- LeWaGAN/MLOps/preprocessing.py
+++ LeWaGAN/MLOps/preprocessing.py
@@ -1,8 +1,9 @@
 import tensorflow
-from LeWaGAN.MLOps.params import IMAGE_SIZE
+from LeWaGAN.MLOps.params import IMAGE_SIZE, ROOT_PATH, DATA_PATH, DATA_LOCATION
+import os
 
 #Create a dataset from a local directory
-def create_dataset(path, image_size=(IMAGE_SIZE, IMAGE_SIZE), labels=None, color_mode='rgb', batch_size=32,
+def create_dataset(data_location = DATA_LOCATION, image_size=(IMAGE_SIZE, IMAGE_SIZE), labels=None, color_mode='rgb', batch_size=32,
                      validation_split=0.) :
     """This function create a dataset for tensorflow from a directory with images
     arguments :
@@ -12,16 +13,19 @@ def create_dataset(path, image_size=(IMAGE_SIZE, IMAGE_SIZE), labels=None, color
     validation_split : float between 0 and 1
     """
 
-    #Instanciate dataset
-    dataset = tensorflow.keras.utils.image_dataset_from_directory(path=path, color_mode=color_mode,
-                                           labels=None, image_size=image_size,
-                                           batch_size=None, validation_split=validation_split)
+    if data_location == 'LOCAL' :
 
-    #Normalization / Scaling
-    func = lambda x : x/255
-    n_dataset = dataset.map(map_func=func)
+        #Instanciate dataset
+        path = os.path.join(ROOT_PATH, DATA_PATH)
+        dataset = tensorflow.keras.utils.image_dataset_from_directory(path, color_mode=color_mode,
+                                            labels=None, image_size=image_size,
+                                            batch_size=None, validation_split=validation_split)
 
-    #Batch the dataset with specified batch_size
-    f_dataset = n_dataset.batch(batch_size=batch_size, drop_remainder=False)
+        #Normalization / Scaling
+        func = lambda x : x/255
+        n_dataset = dataset.map(map_func=func)
 
-    return f_dataset
+        #Batch the dataset with specified batch_size
+        f_dataset = n_dataset.batch(batch_size=batch_size, drop_remainder=False)
+
+        return f_dataset
