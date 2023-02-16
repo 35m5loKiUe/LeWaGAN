4db7bfde44515c44c2059385ccc0cbac08329f86
theo pinto--dalle <pinto.theo@icloud.com>
2023-02-16 21:01:30 +0100

    implemented a function to generate an image while controlling the effect of eigenvectors on the input noise

--- LeWaGAN/MLOps/latent_space.py
+++ LeWaGAN/MLOps/latent_space.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy import linalg as LA
+import model
 
 def get_eigenvectors(matrix, k=10) :
     """This function computes the k most important eigenvectors from
-    a matrix of weight"""
+    a matrix of weight
+
+    it returns a list of k vectors callable by index"""
 
     #Compute AT*A
     A = np.dot(matrix, matrix.transpose())
@@ -18,3 +21,37 @@ def get_eigenvectors(matrix, k=10) :
     vectors = [v[i] for i in sort ]
 
     return np.array(vectors)
+
+
+def generate_with_eigenvectors(alpha=1) :
+    """This function generates an image with control over the noise (with eigenvecotrs)
+    Args :
+    alpha ; 1 by default, scalar to apply same weight to each eigenvector, array of size k to apply
+    different weight to each vectors.
+    """
+    assert isinstance(alpha, (int, float, list, np.array)) == True
+
+    generator = model.make_generator_model()
+
+    #Generate a random noise of shape (100,)
+    noise = np.random.normal([1, 100])
+
+    #Get the weight matrix and compute A*A_T
+    weight_matrix = generator.trainable_variables[0].numpy()
+
+    #Compute eigenvectors (list of k vectors, callable by index)
+    vectors = get_eigenvectors(weight_matrix, k=10)
+
+    #generate a new noise
+    c = 0
+    for vector in vectors :
+        noise += vector*alpha[c]
+        c += 1
+
+    final_noise = noise
+
+
+    #generate an image
+    generated_image = generator(final_noise)
+
+    return generated_image
